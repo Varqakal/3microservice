4a5a6602f6e4ff1c5900754248ea36c26279c8f2
varqa <varqaetobo@gmail.com>
2026-06-10 16:22:36 +0100

    projet contenant les 3 services

---
+++ inventory_service/src/main/java/com/microservice/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.microservice.inventory_service.repository;
+
+import com.microservice.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
---
+++ product-service/src/test/java/com/microserviecs/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,72 @@
+package com.microserviecs.product_service;
+
+import com.microserviecs.product_service.dto.ProductRequest;
+import com.microserviecs.product_service.repository.ProductRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
+import tools.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductServiceApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@BeforeEach
+	void setUp() {
+		productRepository.deleteAll();
+	}
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistrar){
+		dynamicPropertyRegistrar.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+	@Test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString =  objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+					.contentType(String.valueOf(MediaType.APPLICATION_JSON))
+					.content(productRequestString))
+				.andExpect(status().isCreated());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+
+	}
+
+	private ProductRequest getProductRequest(){
+		return ProductRequest.builder()
+				.name("iphone 13")
+				.description("iphone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
+	}
+
+
+}
